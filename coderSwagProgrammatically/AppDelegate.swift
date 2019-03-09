a1d8623c9812afea8f4a3ef0ad2eabb7cd7e7980
Efthimios Vasilopoulos <efthi@Efthimioss-MacBook-Pro.local>
2019-03-09 11:54:43 +0100

    created the setup Function

--- coderSwagProgrammatically/AppDelegate.swift
+++ coderSwagProgrammatically/AppDelegate.swift
@@ -16,16 +16,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        
         // Setting the View
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .white
         
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = UINavigationController(rootViewController: CategoriesVC())
+        
+        setupNavigationBarAppearance()
+        
         return true
     }
+    
+    //MARK: - Setting up the NavigationBar Appearance
+    func setupNavigationBarAppearance() {
+        UINavigationBar.appearance().barTintColor = UIColor(red: 87/255, green: 39/255, blue: 40/255, alpha: 1)
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().tintColor = .white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+    }
+    
+    
 
+
+    //MARK: - Default Functions
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
---
+++ coderSwagProgrammatically/Controller/CategoriesVC.swift
@@ -0,0 +1,32 @@
+//
+//  CategoriesVC.swift
+//  coderSwagProgrammatically
+//
+//  Created by Efthimios Vasilopoulos on 09.03.19.
+//  Copyright © 2019 Efthimios Vasilopoulos. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "CODER SWAG"
+
+
+    }
+
+//    override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return .lightContent
+//    }
+
+}
+
+//extension UINavigationController {
+//    open override var preferredStatusBarStyle: UIStatusBarStyle {
+//        return topViewController?.preferredStatusBarStyle ?? .lightContent
+//    }
+//}
+
