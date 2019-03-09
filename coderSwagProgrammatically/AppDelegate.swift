723ed08fb09e029b16d8f0f4d1c8735127303838
Efthimios Vasilopoulos <efthi@Efthimioss-MacBook-Pro.local>
2019-03-09 10:47:02 +0100

    Deleted the main.storyboard. Created the new View programmatically.

--- coderSwagProgrammatically/AppDelegate.swift
+++ coderSwagProgrammatically/AppDelegate.swift
@@ -15,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        // Setting the View
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .white
+        
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
         return true
     }
 
